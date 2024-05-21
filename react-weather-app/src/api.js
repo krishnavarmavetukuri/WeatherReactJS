471e3e33a9b5680b8fa4fccd3e1f4583808dc05c
Krishna Varma Vetukuri <kvetukur@gitam.in>
2024-05-22 01:20:14 +0530

    My ReactJS project.

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "caaf7a240dmsha807b7cb80ce72dp17c017jsnf0ae69d67737",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "359dc32d708d810fdbb90f0f791c00bb";
